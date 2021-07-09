4974fe5212e4140048b0af1193a0142f6fc24c59
Mikel Larreategi <mlarreategi@codesyntax.com>
2021-07-09 08:27:04 +0200

    black

--- plone_restapi_harvester/cli/cli.py
+++ plone_restapi_harvester/cli/cli.py
@@ -3,17 +3,19 @@ import sys
 from plone_restapi_harvester.harvester.harvester import load_document, harvest_document
 from plone_restapi_harvester.loaders.loaders import FSLoader, PostgresLoader
 
+
 @click.command()
-@click.option('--loader', type=click.Choice(['fs', 'db']), default='fs')
-@click.argument('id')
+@click.option("--loader", type=click.Choice(["fs", "db"]), default="fs")
+@click.argument("id")
 def main(id, loader):
-    if loader == 'db':
+    if loader == "db":
         document_loader = PostgresLoader()
     else:
-        document_loader = FSLoader() 
-    normalized =  harvest_document(load_document(document_loader, id))    
+        document_loader = FSLoader()
+    normalized = harvest_document(load_document(document_loader, id))
     # pass data to the next airflow step
 
-if __name__ == '__main__':
-    #sys.exit(main())  # pragma: no cover
+
+if __name__ == "__main__":
+    # sys.exit(main())  # pragma: no cover
     main()
